3bacc296078e3b05104456394cd819438be0f358
henriquemarinho11 <60720142+henriquemarinho11@users.noreply.github.com>
2022-05-19 20:00:30 -0300

    Teste de Logica

---
+++ TesteLogica_F360/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TesteLogica_F360
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Iniciar.Programa();
+        }
+    }
+}
